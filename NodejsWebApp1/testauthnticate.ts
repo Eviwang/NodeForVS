3ff86857b4e2794ff4a792400bfd80bcc5abb4a3
Evi.Wang <525205537@qq.com>
2017-06-07 15:04:08 +0800

    add file upload

---
+++ NodejsWebApp1/testauthnticate.ts
@@ -0,0 +1,22 @@
+﻿import * as http from "http";
+
+http.createServer(function (req,res) {
+
+    let auth = req.headers["authorization"] || "";
+    let parts = auth.split(" ");
+    let method = parts[0];
+    let encoded = parts[1] || "";
+    let decoded = new Buffer(encoded, "base64").toString("utf-8").split(":");
+
+    let user = decoded[0];
+    let pass = decoded[1];
+
+    if (user == "wang" && pass == "123") {
+        res.write("ok");
+    } else {        
+        res.setHeader("WWW-Authenticate", 'Basic realm="Secure Area"');
+        res.writeHead(401);
+    }
+
+    res.end();
+}).listen(5000);
---
+++ NodejsWebApp1/upload.ts
@@ -0,0 +1,29 @@
+﻿import * as http from "http";
+
+function hasBody(req) {
+    return "transfer-encoding" in req.headers || "content-length" in req.headers;
+}
+
+http.createServer(function (req, res) {
+    let rawBody = null;
+    if (req.method == "POST") {
+        if (hasBody(req)) {
+            let buffers = [];
+            req.on("data", function (chunk) {
+                buffers.push(chunk);
+            });
+            req.on("end", function () {
+                rawBody = Buffer.concat(buffers).toString();
+                console.log("end",rawBody);
+            });
+        }
+    } else {
+        console.log("get hello");
+    }
+    res.write("ok");
+    res.end();
+}).listen(5000);
+
+
+
+
